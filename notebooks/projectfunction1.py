9992969fe504f9df3813dee13ba8da1b7f517db1
julieshim <julieshim.j@gmail.com>
2021-10-30 21:04:19 -0700

    edit1

--- notebooks/projectfunction1.py
+++ notebooks/projectfunction1.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
-def unprocess(csv_file):
+
+def unprocessed(csv_file):
     df=pd.read_csv(csv_file)
     return df
 
 def load_and_process(csv_file):
     df = pd.read_csv(csv_file)
-    df_clean=df.copy().drop(['region'],axis=1)
-    df_clean1=df_clean.copy().drop(['children'],axis=1)
-    df_sorted=df_clean1.sort_values("age")
+    df_clean=df.copy().drop(['region','children'],axis=1)
+    df_sorted=df_clean.sort_values("age")
     return df_sorted
+
 #def Age(dfp):
 #         df1=dfp.groupby('age',as_index=False).sum()
 #         return df1
