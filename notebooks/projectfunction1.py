8425a438bfa521e6a7f128b821d50f8db4d02c0e
julieshim <julieshim.j@gmail.com>
2021-10-31 19:27:41 -0700

    submission draft

--- notebooks/projectfunction1.py
+++ notebooks/projectfunction1.py
@@ -7,28 +7,36 @@ def unprocessed(csv_file):
 
 def load_and_process(csv_file):
     df = pd.read_csv(csv_file)
-    df_clean=df.copy().drop(['region','children'],axis=1)
+    df_clean = df.round(1)
     df_sorted=df_clean.sort_values("age")
     return df_sorted
 
-#def Age(dfp):
-#         df1=dfp.groupby('age',as_index=False).sum()
-#         return df1
+def replace_object(csv_file):
+    df = pd.read_csv(csv_file)
+    df.smoker.replace(('yes', 'no'), (1, 0), inplace=True)
+    df.sex.replace(('female', 'male'), (1, 0), inplace=True)
+    df_clean = df.copy().drop(['region','children'],axis=1)
+    df_clean = df_clean.round(1)
+    # df.rename(columns = {"smoker": "smoking status",}, inplace = True)
+    return df_clean
+
+def byregion(csv_file):
+    df = pd.read_csv(csv_file)
+    import matplotlib.pylab as plt
+    import seaborn as sns
+    axk= plt.figure(figsize=(6,6))
+    sns.lmplot(x='age', y= 'charges',data=df,markers='.',hue ='region',height=10,aspect=1)
+    return plt.title('Responsiveness of the medical bill to age by region')
+
+def bysmoker(csv_file):
+    df = pd.read_csv(csv_file)
+    import matplotlib.pylab as plt
+    import seaborn as sns
+    axk= plt.figure(figsize=(6,6))
+    sns.lmplot(x='age', y= 'charges',data=df,markers='.',hue ='smoker',height=10,aspect=1)
+    return plt.title('Responsiveness of the medical bill to age by smoking status')
+
 def Age(dfp):
     df = dfp.groupby('age', as_index=False).sum()
     return df
-
-def bmia(dfp):
-    df20 = dfp.groupby('bmi', as_index=False).sum()
-    return df20
-def sort_age(dfp):
-    df1=dfp.groupby("age",as_index=False).sum()
-    for i in range (18,64):
-        df2=pd.melt(df1,id_vars='age',value_vars=[i])
-        return df2
-def sort_genre(dfp):
-    df1 = dfp.groupby("age", as_index=False).sum()
-    df2 = pd.melt(df1, id_vars="bmi", value_vars=["charges"])
-    return df2
-                    
                     
